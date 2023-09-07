786c794b53ab5451ca4e4d579ecca320c174383d
nourahmed <105996059+nour-elhabbal@users.noreply.github.com>
2023-09-07 03:01:18 +0300

    code fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import TotalInfoCardsList from "./components/TotalInfoCardsList";
-// import "./CSS/defaultStyles.css";
 import "./CSS/App.scss";
 import "./CSS/darkTheme.scss";
-import "./CSS/lightTheme.scss";
 import Header from "./components/Header";
 import OverviewCardsList from "./components/OverviewCardsList";
 
